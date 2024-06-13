a54e630788a405aa7719250842ab4a869cdd39e1
Tran Vuong Quoc Dat <tranvuongquocdat@gmail.com>
2024-06-13 13:37:17 +0700

    add cf3

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "Johnny",
+    name: "MBA Case",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "",
     bio: "educational and professional case study",
